59dcac36501bd8c824bfb42e816d2b254275f264
cheryl e <kimyerin@yahoo.com>
2018-11-04 20:13:19 +0000

    automatically backed up by learn

--- badges.rb
+++ badges.rb
@@ -2,6 +2,6 @@ def badge_maker (name)
   puts "Hell, my name is ${name}."
 end
 
-def batch_badge_creater
+def batch_badge_creater (name)
   name = [" Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 end
