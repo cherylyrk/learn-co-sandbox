ec2b8575c024b9747c990180652b500f35154f16
cheryl e <kimyerin@yahoo.com>
2018-11-04 20:41:17 +0000

    automatically backed up by learn

--- badges.rb
+++ badges.rb
@@ -15,7 +15,7 @@ def batch_badge_creater (name)
 end
 
 def assign_rooms (name)
-  name.each_with_index {|name, index| puts "Hello, #{name}! You'll be assigned to room "
+  name.each_with_index {|name, index| puts "Hello, #{name}! You'll be assigned to room #{index +1}!"
 end
 
 def printer (name)
