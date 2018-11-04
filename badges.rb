8a4ac77c9b21ddc2e2a5c09743c40e1a98aa783f
cheryl e <kimyerin@yahoo.com>
2018-11-04 20:25:50 +0000

    automatically backed up by learn

--- badges.rb
+++ badges.rb
@@ -5,6 +5,12 @@ end
 def batch_badge_creater (name)
   name = [" Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #{name[0]}."
+  puts "Hello, my name is #{name[1]}."
+  puts "Hello, my name is #{name[2]}."
+  puts "Hello, my name is #{name[3]}."
+  puts "Hello, my name is #{name[4]}."
+  puts "Hello, my name is #{name[5]}."
+  puts "Hello, my name is #{name[6]}."
 end
 
 def assign_rooms (name)
